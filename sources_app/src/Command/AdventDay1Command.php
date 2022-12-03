9c4045151f61c849103e8187bf2d81fe1297ad8f
Nicolas Guérinet <n.guerinet@groupeonepoint.com>
2022-12-03 15:23:18 +0100

    reformat and add php cs fixer

--- sources_app/src/Command/AdventDay1Command.php
+++ sources_app/src/Command/AdventDay1Command.php
@@ -11,43 +11,43 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 const MAIN_DATA_DIR = '/var/data/';
 #[AsCommand(
-  name: 'advent:day1',
-  description: 'Advent day 1',
+    name: 'advent:day1',
+    description: 'Advent day 1',
 )]
 class AdventDay1Command extends Command
 {
-  protected function configure(): void
-  {
-    $this
-      ->addOption('file', '-f', InputOption::VALUE_REQUIRED, 'file value');
-  }
-
-  protected function execute(InputInterface $input, OutputInterface $output): int
-  {
-    $io = new SymfonyStyle($input, $output);
-
-    $inputFilePath = $input->getOption('file');
-    if (!$inputFilePath) {
-      $inputFilePath = MAIN_DATA_DIR . 'day1/input';
+    protected function configure(): void
+    {
+        $this
+          ->addOption('file', '-f', InputOption::VALUE_REQUIRED, 'file value');
     }
-    $io->note(sprintf('Input file path is : %s', $inputFilePath));
 
-    $contentInput = file($inputFilePath);
-    $totalElfCarriedCalories = 0;
-    $tabCalories = [];
-    foreach ($contentInput as $contentLine) {
-      $bagCalories = intval($contentLine);
-      if ($bagCalories === 0) {
-        $tabCalories[] = $totalElfCarriedCalories;
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $inputFilePath = $input->getOption('file');
+        if (!$inputFilePath) {
+            $inputFilePath = MAIN_DATA_DIR.'day1/input';
+        }
+        $io->note(sprintf('Input file path is : %s', $inputFilePath));
+
+        $contentInput = file($inputFilePath);
         $totalElfCarriedCalories = 0;
-      } else {
-        $totalElfCarriedCalories += $bagCalories;
-      }
-    }
-    rsort($tabCalories);
-    $io->info(sprintf('Better elf is carrying %s calories', $tabCalories[0]));
-    $io->info(sprintf('3 elves carried max calories for %s', $tabCalories[0] + $tabCalories[1] + $tabCalories[2]));
+        $tabCalories = [];
+        foreach ($contentInput as $contentLine) {
+            $bagCalories = intval($contentLine);
+            if (0 === $bagCalories) {
+                $tabCalories[] = $totalElfCarriedCalories;
+                $totalElfCarriedCalories = 0;
+            } else {
+                $totalElfCarriedCalories += $bagCalories;
+            }
+        }
+        rsort($tabCalories);
+        $io->info(sprintf('Better elf is carrying %s calories', $tabCalories[0]));
+        $io->info(sprintf('3 elves carried max calories for %s', $tabCalories[0] + $tabCalories[1] + $tabCalories[2]));
 
-    return Command::SUCCESS;
-  }
+        return Command::SUCCESS;
+    }
 }
--- sources_app/src/Command/AdventDay2Command.php
+++ sources_app/src/Command/AdventDay2Command.php
@@ -2,7 +2,6 @@
 
 namespace App\Command;
 
-
 const ROCK_X = 'A';
 const ROCK_Y = 'X';
 const PAPER_X = 'B';
@@ -29,90 +28,95 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Routing\Exception\InvalidParameterException;
 
-
 #[AsCommand(
-  name: 'advent:day2',
-  description: 'Advent day 2',
+    name: 'advent:day2',
+    description: 'Advent day 2',
 )]
 class AdventDay2Command extends Command
 {
-  protected function configure(): void
-  {
-    $this
-      ->addOption('file', '-f', InputOption::VALUE_REQUIRED, 'file value');
-  }
-
-  protected function execute(InputInterface $input, OutputInterface $output): int
-  {
-    $io = new SymfonyStyle($input, $output);
-
-    $contentInput = file('/var/data/day2/input');
-    $part1Score = 0;
-    $part2Score = 0;
-    foreach ($contentInput as $contentLine) {
-      $game = explode(" ", trim($contentLine));
-
-      $part1Score += $this->calculateStrategy1($game[0], $game[1]) + $this->getScorePlayed($game[1]);
-      $part2Score += $this->calculateStrategy2($game[0], $game[1]);
+    protected function configure(): void
+    {
+        $this
+            ->addOption('file', '-f', InputOption::VALUE_REQUIRED, 'file value');
     }
-    $io->info(sprintf("Your score %s with strategy1", $part1Score));
-    $io->info(sprintf("Your score %s with strategy2", $part2Score));
 
-    return Command::SUCCESS;
-  }
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
 
-  private function getScorePlayed(string $played): int
-  {
-    return match ($played) {
-      ROCK_Y => ROCK_SCORE,
-      PAPER_Y => PAPER_SCORE,
-      SCISSOR_Y => SCISSOR_SCORE,
-      default => throw new InvalidParameterException('invalid game chose PAPER, ROCK or SCISSOR')
-    };
-  }
+        $contentInput = file('/var/data/day2/input');
+        $part1Score = 0;
+        $part2Score = 0;
+        foreach ($contentInput as $contentLine) {
+            $game = explode(' ', trim($contentLine));
 
-  private function calculateStrategy1(string $player, string $me): int
-  {
-    return match ($player) {
-      ROCK_X => match ($me) {
-        ROCK_Y => DRAW_SCORE,
-        PAPER_Y => WIN_SCORE,
-        SCISSOR_Y => LOSE_SCORE,
-        default => throw new InvalidParameterException('invalid game chose PAPER, ROCK or SCISSOR for game played')
-      },
-      PAPER_X => match ($me) {
-        ROCK_Y => LOSE_SCORE,
-        PAPER_Y => DRAW_SCORE,
-        SCISSOR_Y => WIN_SCORE,
-        default => throw new InvalidParameterException('invalid game chose PAPER, ROCK or SCISSOR  for game played')
-      },
-      SCISSOR_X => match ($me) {
-        ROCK_Y => WIN_SCORE,
-        PAPER_Y => LOSE_SCORE,
-        SCISSOR_Y => DRAW_SCORE,
-        default => throw new InvalidParameterException('invalid game chose PAPER, ROCK or SCISSOR  for game played')
-      }
-    };
-  }
-
-  private function calculateStrategy2(string $player, string $gameResult): int
-  {
-    return match ($gameResult) {
-      WIN => WIN_SCORE + match ($player) {
-          SCISSOR_X => ROCK_SCORE,
-          ROCK_X => PAPER_SCORE,
-          PAPER_X => SCISSOR_SCORE,
-        },
-      DRAW => DRAW_SCORE + match ($player) {
-          SCISSOR_X => SCISSOR_SCORE,
-          ROCK_X => ROCK_SCORE,
-          PAPER_X => PAPER_SCORE,
-        },
-      LOSE => LOSE_SCORE + match ($player) {
-          SCISSOR_X => PAPER_SCORE,
-          ROCK_X => SCISSOR_SCORE,
-          PAPER_X => ROCK_SCORE,
+            $part1Score += $this->calculateStrategy1($game[0], $game[1]) + $this->getScorePlayed($game[1]);
+            $part2Score += $this->calculateStrategy2($game[0], $game[1]);
         }
-    };
-  }
+        $io->info(sprintf('Your score %s with strategy1', $part1Score));
+        $io->info(sprintf('Your score %s with strategy2', $part2Score));
+
+        return Command::SUCCESS;
+    }
+
+    private function getScorePlayed(string $played): int
+    {
+        return match ($played) {
+            ROCK_Y => ROCK_SCORE,
+            PAPER_Y => PAPER_SCORE,
+            SCISSOR_Y => SCISSOR_SCORE,
+            default => throw new InvalidParameterException('invalid game chose PAPER, ROCK or SCISSOR')
+        };
+    }
+
+    private function calculateStrategy1(string $player, string $me): int
+    {
+        return match ($player) {
+            ROCK_X => match ($me) {
+                ROCK_Y => DRAW_SCORE,
+                PAPER_Y => WIN_SCORE,
+                SCISSOR_Y => LOSE_SCORE,
+                default => throw new InvalidParameterException(
+                    'invalid game chose PAPER, ROCK or SCISSOR for game played'
+                )
+            },
+            PAPER_X => match ($me) {
+                ROCK_Y => LOSE_SCORE,
+                PAPER_Y => DRAW_SCORE,
+                SCISSOR_Y => WIN_SCORE,
+                default => throw new InvalidParameterException(
+                    'invalid game chose PAPER, ROCK or SCISSOR  for game played'
+                )
+            },
+            SCISSOR_X => match ($me) {
+                ROCK_Y => WIN_SCORE,
+                PAPER_Y => LOSE_SCORE,
+                SCISSOR_Y => DRAW_SCORE,
+                default => throw new InvalidParameterException(
+                    'invalid game chose PAPER, ROCK or SCISSOR  for game played'
+                )
+            }
+        };
+    }
+
+    private function calculateStrategy2(string $player, string $gameResult): int
+    {
+        return match ($gameResult) {
+            WIN => WIN_SCORE + match ($player) {
+                    SCISSOR_X => ROCK_SCORE,
+                    ROCK_X => PAPER_SCORE,
+                    PAPER_X => SCISSOR_SCORE,
+                },
+            DRAW => DRAW_SCORE + match ($player) {
+                    SCISSOR_X => SCISSOR_SCORE,
+                    ROCK_X => ROCK_SCORE,
+                    PAPER_X => PAPER_SCORE,
+                },
+            LOSE => LOSE_SCORE + match ($player) {
+                    SCISSOR_X => PAPER_SCORE,
+                    ROCK_X => SCISSOR_SCORE,
+                    PAPER_X => ROCK_SCORE,
+                }
+        };
+    }
 }
